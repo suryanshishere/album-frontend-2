a3927cfcc6bc88b384927f4b5b991830b32d9d64
suryanshishere <heresuryanshsingh@gmail.com>
2025-01-24 19:13:41 +0530

    done

--- src/app/photoAlbum.tsx
+++ src/app/photoAlbum.tsx
@@ -2,17 +2,17 @@
 
 import React from "react";
 import HTMLFlipBook from "react-pageflip";
-
+import Image from "next/image";
 import IMAGES from "./images.json";
 
 export default function PhotoAlbum() {
   return (
     <div className="h-full w-full absolute flex items-center justify-center ">
-       {/* @ts-expect-error */}
-       <HTMLFlipBook width={300} height={400} showCover autoSize drawShadow>
+      {/* @ts-expect-error: Ignoring type error due to incompatible types from react-pageflip */}
+      <HTMLFlipBook width={300} height={400} showCover autoSize drawShadow>
         {IMAGES.map((image, index) => (
           <div key={index} className="page">
-            <img
+            <Image
               src={image.src}
               alt={image.title}
               className="h-full w-full object-cover rounded-lg"
